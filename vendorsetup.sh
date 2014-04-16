bbaf943d1dd458ca2551fb543e4a12543f4dcb83
Willi Ye <williye97@gmail.com>
2014-04-16 16:53:36 +0200

    remove eng and full builds
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,8 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_i9103-eng
-add_lunch_combo full_i9103-eng
 add_lunch_combo cm_i9103-userdebug
 
 echo "Apply patch to frameworks/base"
